65687d69b7c0b875e86f92e50c227717c72e5efd
Evan Wiorek <124269000+EvanWiorek@users.noreply.github.com>
2023-05-31 12:19:35 -0500

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/global.css';
+// import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -19,15 +19,6 @@ const firstPost = () => {
         /> */}
         </Head>
         <h1>First Post</h1>
-        <h2>
-          <Link href="/">Back to home</Link>
-        </h2>
-        <Image
-          src="/images/profile.jpg" // Route of the image file
-          height={144} // Desired size with correct aspect ratio
-          width={144} // Desired size with correct aspect ratio
-          alt="Your Name"
-        />
       </Layout>
     </>
   );
